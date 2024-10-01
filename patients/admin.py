9bbb8a7092272c0aad010654ab75941d2bef8916
Nohan ahmed <mdpolashislam152@gmail.com>
2024-10-02 03:58:30 +0600

    Create patient model

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "contact_number", "blood_type")
+    search_fields = ("id", "user", "contact_number", "blood_type")
+    list_filter = ("blood_type",)
--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import User
+from .constraints import BLOOD_GROUP_CHOICES
+
 
 # Create your models here.
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="patient")
+    contact_number = models.CharField(max_length=17)
+    blood_type = models.CharField(choices=BLOOD_GROUP_CHOICES, max_length=3)
+    insurance_provider = models.CharField(max_length=250, null=True, blank=True)
+    insurance_number = models.CharField(max_length=50, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f'{self.user.first_name} {self.user.last_name}'
