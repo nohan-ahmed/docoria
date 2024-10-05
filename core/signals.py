d306cdfa753f8a2d31d03dbf8165a2b37b365795
Nohan ahmed <mdpolashislam152@gmail.com>
2024-10-06 01:52:00 +0600

    Notes for django signals. this note is available on inside the core app's signals.py file

--- core/signals.py
+++ core/signals.py
@@ -13,3 +13,11 @@ def create_patient(sender, instance, created, **kwargs):
         Patient.objects.create(user=instance)
 
 
+# =================================================
+# Notes:- for django signals
+# =================================================
+# pre_save and post_save: Triggered before or after a model instance is saved.
+# pre_delete and post_delete: Triggered before or after a model instance is deleted.
+# Yes, you need to ensure that your Django signal is properly registered in your apps.py file's ready()
+# -------------------------------------------------
+# sender হলো সেই model জার কনো instance কে save() করলে আমদের receiver function টা triggered হবে। অ্যান্ড instance হলো সে যাকে save() করা হয়েছে।
