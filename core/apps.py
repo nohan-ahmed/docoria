de8e137accc62de279943578845fc3a8aac1c9cf
Nohan ahmed <mdpolashislam152@gmail.com>
2024-10-06 22:18:25 +0600

    Move patient's signals file to the patients model

--- core/apps.py
+++ core/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-    
-    # Import the signals.py in apps.py: You need to tell Django to import the signals.py file when your app is ready so that the signal gets registered.
-    def ready(self):
-        import core.signals
--- patients/apps.py
+++ patients/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PatientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'patients'
+    # Import the signals.py in apps.py: You need to tell Django to import the signals.py file when your app is ready so that the signal gets registered.
+    def ready(self):
+        import patients.signals
